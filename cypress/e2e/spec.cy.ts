a06296b7d9625d674ee1083b83d526e9659c8988
Melissa Rose <123melissa321@gmail.com>
2022-09-26 08:55:36 +0100

    yarn added cypress and cypress types, tried to add an always-passing test (see /cypress/e2e/spec.cy.ts)

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,16 @@
+import { expect } from 'cypress';
+
+//Default test given by cypress:
+
+// describe('empty spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+
+// example test given on cypress website (should always pass)
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
