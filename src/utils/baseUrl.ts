b0423a0b4cf99ca3e860377c20efea246a2e208b
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-24 15:41:55 +0000

    Update front and back urls

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,9 +1,9 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://c5c1-backend.herokuapp.com"
+    ? "https://resource-sharer.onrender.com"
     : "http://localhost:4000";
 
 export const frontEndUrl =
   process.env.NODE_ENV === "production"
-    ? "https://c5c1-frontend.netlify.app/"
+    ? "https://resource-sharer.netlify.app/"
     : "http://localhost:3000";
