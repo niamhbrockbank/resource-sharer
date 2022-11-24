2edc649ed45abfb2e4cbef083750945631f6d017
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-24 16:15:48 +0000

    Temporarily connect to deployed backend

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,9 +1,13 @@
-export const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://resource-sharer.onrender.com"
-    : "http://localhost:4000";
+// export const baseUrl =
+//   process.env.NODE_ENV === "production"
+//     ? "https://resource-sharer.onrender.com"
+//     : "http://localhost:4000";
 
-export const frontEndUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://resource-sharer.netlify.app/"
-    : "http://localhost:3000";
+// export const frontEndUrl =
+//   process.env.NODE_ENV === "production"
+//     ? "https://resource-sharer.netlify.app/"
+//     : "http://localhost:3000";
+
+export const baseUrl = "https://resource-sharer.onrender.com"
+
+export const frontEndUrl = "https://resource-sharer.netlify.app/"
