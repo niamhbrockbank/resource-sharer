da174403c8395670d1ac50dd4f6c4e2b6d68549d
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-27 17:46:02 +0000

    Add search icon

--- src/components/SearchAndFilter/SearchBar.tsx
+++ src/components/SearchAndFilter/SearchBar.tsx
@@ -9,15 +9,16 @@ export default function SearchBar({
 }: ISearchBarProps): JSX.Element {
   return (
     <>
-      <input
-        id="search_bar"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Search"
-      />
-      {/* TODO: Add search bar magnifying glass icon to RHS */}
-      {/* <img src='./img/search.svg' alt='search button'/> */}
-      {/* TODO: When search move the results up the page/ hide the top resources */}
+        <input
+          id="search_bar"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search"
+        />
+        <img id='search_icon' src='./img/search.svg' alt='search icon'/>
+        {/* TODO: Add search bar magnifying glass icon to RHS */}
+        {/* <img src='./img/search.svg' alt='search button'/> */}
+        {/* TODO: When search move the results up the page/ hide the top resources */}
     </>
   );
 }
