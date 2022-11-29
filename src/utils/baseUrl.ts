beaa99a3a8720995da069786214e6a082e06d804
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-29 17:47:25 +0000

    Update baseUrl to depend on production env

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,13 +1,9 @@
-// export const baseUrl =
-//   process.env.NODE_ENV === "production"
-//     ? "https://resource-sharer.onrender.com"
-//     : "http://localhost:4000";
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://resource-sharer.onrender.com"
+    : "http://localhost:4000";
 
-// export const frontEndUrl =
-//   process.env.NODE_ENV === "production"
-//     ? "https://resource-sharer.netlify.app/"
-//     : "http://localhost:3000";
-
-export const baseUrl = "http://localhost:4000";
-
-export const frontEndUrl = "https://resource-sharer.netlify.app";
+export const frontEndUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://resource-sharer.netlify.app/"
+    : "http://localhost:3000";
